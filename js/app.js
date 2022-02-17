2d58d6598d57d9ce695272c23bb6b47608abc660
iamraihan <trueraihan@gmail.com>
2022-02-17 14:39:26 +0600

    validation aded

--- js/app.js
+++ js/app.js
@@ -10,23 +10,65 @@ let saveInput = document.getElementById('save-input')
 let saveAmount = document.getElementById('save-amount')
 let remainBalance = document.getElementById('remaining-balance') */
 
+
+
 // event listener
 document.getElementById('calculate-btn').addEventListener('click', function () {
+
     let incomeInput = document.getElementById('income-input')
 
     let foodInput = document.getElementById('food-input')
     let rentInput = document.getElementById('rent-input')
     let clothInput = document.getElementById('cloth-input')
-    let totalExpenses = document.getElementById('total-expense')
-    const expenses = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothInput.value)
-    totalExpenses.innerText = expenses
+    calculateAmount(foodInput, rentInput, clothInput)
 
-    let balance = document.getElementById('balance').innerText = incomeInput.value - totalExpenses.innerText
-
-
-    document.getElementById('save-button').addEventListener('click', function () {
-        let saveInput = document.getElementById('save-input').value
-        let saveAmount = document.getElementById('save-amount').innerText = incomeInput.value * saveInput / 100
-        document.getElementById('remaining-balance').innerText = balance - saveAmount
-    })
 })
+
+function calculateAmount(num1, num2, num3) {
+    let balance = document.getElementById('balance')
+
+    let incomeInput = document.getElementById('income-input')
+    let totalExpense = document.getElementById('total-expense')
+    let expenses = parseFloat(num1.value) + parseFloat(num2.value) + parseFloat(num3.value)
+
+
+    if (isNaN(expenses) || expenses < 0) {
+        expenses = ''
+        return alert('input a valid number')
+    }
+    if (incomeInput.value < expenses) {
+        return alert('expense cant be more than income')
+    }
+
+    totalExpense.innerText = expenses
+    balance.innerText = incomeInput.value - expenses
+}
+
+
+document.getElementById('save-button').addEventListener('click', function () {
+
+    // let balance = document.getElementById('balance')
+
+    let saveAmount = document.getElementById('save-amount')
+    let incomeInput = document.getElementById('income-input')
+    let saveInput = document.getElementById('save-input')
+    let remainBalance = document.getElementById('remaining-balance')
+
+    if (isNaN(saveInput.value) || saveInput.value < 0) {
+        expenses = ''
+        return alert('input a valid number')
+    }
+    if (saveAmount.innerText > remainBalance.innerText) {
+        return alert('cant save more then Remain Balance')
+    }
+
+    saveAmount.innerText = incomeInput.value * saveInput.value / 100
+
+
+    remainBalance.innerText = balance.innerText - saveAmount.innerText
+
+
+
+})
+
+
